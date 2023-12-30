bce26012a6f220eccc89f1bcb73522aadfb6354a
Tales Aguiar <talesdd@outlook.com>
2023-12-30 08:17:37 -0300

    first and last commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+@vite(['resources/sass/app.scss','resources/js/app.js'])
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}}</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+<div class="container">
+
+    <h1>Series</h1>
+
+    {{$slot}}
+
+</div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/series', [\App\Http\Controllers\SeriesController::class, 'index']);
+Route::get('/series/criar', [\App\Http\Controllers\SeriesController::class, 'create']);
+Route::post('/series/salvar',[\App\Http\Controllers\SeriesController::class, 'store']);
